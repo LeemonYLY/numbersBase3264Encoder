99dae69a4bf29296e920cadac8ce44406df248d0
liwenchu <907630605@qq.com>
2023-11-17 11:48:44 +0800

    correct some wrong comments

--- numbersBase3264Encoder.java
+++ numbersBase3264Encoder.java
@@ -3,12 +3,13 @@ package numbersBase3264Encoder;
 import java.io.UnsupportedEncodingException;
 import java.util.Base64;
 
-//输入两个数，以一定概率对两个值的和进行base64或base32编码
+// Enter two numbers
+// encode the sum of the two values with a certain probability
 public class numbersBase3264Encoder {
 	private static final char[] BASE32_CHARS = "ABCDEFGHIJKLMNOPQRSTUVWXYZ234567".toCharArray();
 
 	public static void main(String[] args) {
-		// 2个参数范围均为[0,100000]
+		// Both parameters range [0,100000]
 		double a = Double.parseDouble(args[0]);
 		double b = Double.parseDouble(args[1]);
 
@@ -37,7 +38,7 @@ public class numbersBase3264Encoder {
 			}
 		}
 
-		// 处理剩余的不足5位的位
+		// Handles the remaining bits of less than 5 bits
 		if (bitCount > 0) {
 			int index = (bitBuffer << (5 - bitCount)) & 0x1F;
 			encoded.append(BASE32_CHARS[index]);
@@ -67,17 +68,18 @@ public class numbersBase3264Encoder {
 		}
 
 		if (random < probability) {
-			// 使用base64编码
+			// Use base64 encoding
 			result = base64encodedString.hashCode();
 		} else {
-			// 使用base32编码
+			// Use base32 encoding
 			result = base32encodedString.hashCode();
 		}
 
 		return result;
 	}
 
-	// 当数字绝对值太大时，不断除以2，直到绝对值<=5000
+	// When the absolute value of the number is too large, keep dividing by 2 until
+	// the absolute value <=1000
 	public static int scale(int value) {
 		while (value > 1000 || value < -1000) {
 			value /= 2;
